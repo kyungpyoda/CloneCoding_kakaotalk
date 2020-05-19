9e9842c140be922d6b4ca0bd190531bd96c76559
kyungpyoda <qorghdl@naver.com>
2020-05-19 23:40:31 +0900

    Update main

--- jscript.js
+++ jscript.js
@@ -1,8 +1,20 @@
 function getChatList() {
     document.write('<li>');
+    document.write('<input type="button" ondblclick="chatOpen()">');
+    document.write('</li>');
     document.write('<li>');
-    document.write('<li>');
-    document.write('<li>');
-    document.write('<li>');
-    document.write('<li>');
+    document.write('<input type="button" ondblclick="chatOpen()">');
+    document.write('</li>');    document.write('<li>');
+    document.write('<input type="button" ondblclick="chatOpen()">');
+    document.write('</li>');    document.write('<li>');
+    document.write('<input type="button" ondblclick="chatOpen()">');
+    document.write('</li>');    document.write('<li>');
+    document.write('<input type="button" ondblclick="chatOpen()">');
+    document.write('</li>');    document.write('<li>');
+    document.write('<input type="button" ondblclick="chatOpen()">');
+    document.write('</li>');
+
+}
+function chatOpen() {
+    alert('test');
 }
